40010ffdee5c1526a9a7909144342005a38aa851
Anis Barghouthi <anis.barghouthi@outlook.com>
2021-12-31 11:28:35 +0100

    init PizzaSeervice class.

---
+++ BlazingPizza/Data/PizzaService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        // Call your data access technology here
+    }
+}
