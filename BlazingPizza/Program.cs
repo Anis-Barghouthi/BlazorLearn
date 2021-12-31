5efff7697663ce5a919ec499064095d1cf104f64
Anis Barghouthi <anis.barghouthi@outlook.com>
2021-12-31 21:12:02 +0100

    Initialize the database in Program.cs

--- BlazingPizza/Program.cs
+++ BlazingPizza/Program.cs
@@ -1,4 +1,5 @@
 using BlazingPizza.Data;
+using BlazingPizza;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,5 +22,16 @@ app.UseRouting();
 app.MapRazorPages();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+// Initialize the database
+var scopeFactory = app.Services.GetRequiredService<IServiceScopeFactory>();
+using (var scope = scopeFactory.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<PizzaStoreContext>();
+    if (db.Database.EnsureCreated())
+    {
+        SeedData.Initialize(db);
+    }
+}
 
 app.Run();
